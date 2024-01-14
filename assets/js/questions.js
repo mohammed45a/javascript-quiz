f9dcf5d5568475be55dd87e73ad10d8f276f63ae
Mohammed <maboobakar17@gmail.com>
2024-01-14 19:13:46 +0000

    added the questions and answers for the quiz to the questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+//////// QUESTIONS, OPTIONS AND CORRECT ANSWER //////////
+let choices = [
+    {
+        questionTitles: "What is javascript primarily used for?",
+        options: ["Styling web pages", "Creating database tables", "Enhancing interactivity on websites", " Managing server-side operations"],
+        correctAnswer: "Enhancing interactivity on websites"
+    },
+    {
+        questionTitles: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        options: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script file='xxx.js'>"],
+        correctAnswer: "<script src='xxx.js'>"
+    },
+    {
+        questionTitles: "What is the difference between let, const, and var when declaring variables in JavaScript?",
+        options: ["They are interchangeable and can be used in any context", "let and const have block scope, while var has function scope", "let and var are constants, while const is a variable", "They are different syntaxes for the same variable declaration"],
+        correctAnswer: "let and const have block scope, while var has function scope"
+    },
+    {
+        questionTitles: "Arrays in JavaScript can be used to store ____.",
+        options: ["numbers and strings", "other arrays", "booleans", "all can be used"],
+        correctAnswer: "all can be used"
+    },
+    {
+        questionTitles: "Inside which HTML element do we put the JavaScript?",
+        options: ["<scripting>", "<javascript>", "<js>", "<script>"],
+        correctAnswer: "<script>"
+    },
+    {
+        questionTitles: "How do you call a function named " + '"myFunction"?',
+        options: ["call function myFunction()", "call myFunction()", "myFunction()", "myFunction(call)"],
+        correctAnswer: "myFunction()"
+    },
+    {
+        questionTitles: "How can you add a comment in JavaScript?",
+        options: ["<!-- This is a comment -->", "// This is a comment", "' This is a comment", "- This is a comment"],
+        correctAnswer: "// This is a comment"
+    },
+]
